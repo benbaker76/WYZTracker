38da3b97824b8ea4b2211b2b715ecafbb71f170a
Craig Hackney <iratahack@digitalxfer.com>
2021-11-09 20:05:40 -0800

    Added 'Hello, World!'

--- AsmPlayer/demo.asm
+++ AsmPlayer/demo.asm
@@ -1,9 +1,9 @@
-        public  _main
+        public  _music
 
         extern  wyz_player_init
         extern  wyz_play_song
         extern  wyz_play_frame
-_main:
+_music:
         call    wyz_player_init
 
         xor     a
---
+++ AsmPlayer/demo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+extern void music(void);
+
+void main(void)
+{
+	printf("Hello, world!\n");
+	music();
+}
